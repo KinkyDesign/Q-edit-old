1cc8f808104b0f850c5f63d4726a4c4f09a2b8ce
Sopasakis Pantelis <chvng@mail.ntua.gr>
2010-08-24 17:52:31 +0300

    Image Scaling Experiments

--- src/qedit/export/PDFReporter.java
+++ src/qedit/export/PDFReporter.java
@@ -12,12 +12,22 @@ import com.itextpdf.text.Phrase;
 import com.itextpdf.text.html.simpleparser.IncTable;
 import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
+import com.mortennobel.imagescaling.ResampleOp;
 import java.awt.Graphics;
+import java.awt.GraphicsConfiguration;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.HeadlessException;
+import java.awt.Transparency;
 import java.awt.image.AreaAveragingScaleFilter;
+import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
+import java.awt.image.PixelGrabber;
 import java.io.IOException;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.ImageIcon;
 import qedit.clients.components.AdequacyInfo;
 import qedit.clients.components.Author;
 import qedit.clients.components.Compound;
@@ -210,10 +220,11 @@ public class PDFReporter {
 
             for (Compound anal : analogues) {
                 structuralAnalogues.addCell(new PdfPCell(new Phrase(anal.getCasRn(), NORMAL_FONT)));
-                try {   
-                    structuralAnalogues.addCell(new PdfPCell(
-                            Image.getInstance(anal.getUserIcon().getImage().getScaledInstance(60, -1, java.awt.Image.SCALE_AREA_AVERAGING),
-                            java.awt.Color.BLACK)));
+                try {
+                    ResampleOp resampleOp = new ResampleOp(60, 60);
+                    BufferedImage im = resampleOp.filter(toBufferedImage(anal.getUserIcon().getImage()), null);
+
+                    structuralAnalogues.addCell(new PdfPCell(Image.getInstance(im, java.awt.Color.BLACK)));
                 } catch (BadElementException ex) {
                     Logger.getLogger(PDFReporter.class.getName()).log(Level.SEVERE, null, ex);
                 } catch (IOException ex) {
@@ -257,6 +268,78 @@ public class PDFReporter {
             pdf.addElement(new MyParagraph(new Chunk(adequacy.getConclusion(), NORMAL_FONT)).applyIndent(20));
         }
         return pdf;
+    }
+
+    // This method returns true if the specified image has transparent pixels
+    public static boolean hasAlpha(java.awt.Image image) {
+        // If buffered image, the color model is readily available
+        if (image instanceof BufferedImage) {
+            BufferedImage bimage = (BufferedImage) image;
+            return bimage.getColorModel().hasAlpha();
+        }
+
+        // Use a pixel grabber to retrieve the image's color model;
+        // grabbing a single pixel is usually sufficient
+        PixelGrabber pg = new PixelGrabber(image, 0, 0, 1, 1, false);
+        try {
+            pg.grabPixels();
+        } catch (InterruptedException e) {
+        }
+
+        // Get the image's color model
+        ColorModel cm = pg.getColorModel();
+        return cm.hasAlpha();
+    }
+
+    // This method returns a buffered image with the contents of an image
+    public static BufferedImage toBufferedImage(java.awt.Image image) {
+        if (image instanceof BufferedImage) {
+            return (BufferedImage) image;
+        }
+
+        // This code ensures that all the pixels in the image are loaded
+        image = new ImageIcon(image).getImage();
+
+        // Determine if the image has transparent pixels; for this method's
+        // implementation, see Determining If an Image Has Transparent Pixels
+        boolean hasAlpha = hasAlpha(image);
+
+        // Create a buffered image with a format that's compatible with the screen
+        BufferedImage bimage = null;
+        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        try {
+            // Determine the type of transparency of the new buffered image
+            int transparency = Transparency.OPAQUE;
+            if (hasAlpha) {
+                transparency = Transparency.BITMASK;
+            }
+
+            // Create the buffered image
+            GraphicsDevice gs = ge.getDefaultScreenDevice();
+            GraphicsConfiguration gc = gs.getDefaultConfiguration();
+            bimage = gc.createCompatibleImage(
+                    image.getWidth(null), image.getHeight(null), transparency);
+        } catch (HeadlessException e) {
+            // The system does not have a screen
+        }
+
+        if (bimage == null) {
+            // Create a buffered image using the default color model
+            int type = BufferedImage.TYPE_INT_RGB;
+            if (hasAlpha) {
+                type = BufferedImage.TYPE_INT_ARGB;
+            }
+            bimage = new BufferedImage(image.getWidth(null), image.getHeight(null), type);
+        }
+
+        // Copy image to buffered image
+        Graphics g = bimage.createGraphics();
+
+        // Paint the image onto the buffered image
+        g.drawImage(image, 0, 0, null);
+        g.dispose();
+
+        return bimage;
     }
 
     private static class MyParagraph extends Paragraph {
